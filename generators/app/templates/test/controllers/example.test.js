c6a180758946622c8770d578c2005f95f5316cbc
Simon Lamb <simonwlamb@gmail.com>
2018-02-14 12:14:03 +0000

    - Package tidy: Remove sinon-as-promised / supertest-as-promised (no longer required with latest supertest) - Update example.test.js - Mocha v4 will not automatically exit tests (enforces clean up) added --exit to mocha.opts so that the test behaves like v3 Mocha

--- generators/app/templates/test/controllers/example.test.js
+++ generators/app/templates/test/controllers/example.test.js
@@ -1,64 +1,58 @@
 'use strict';
 
-const mocha    = require('mocha');
-const should   = require('chai').should();
-const sinon    = require('sinon');
-const promised = require('sinon-as-promised');
-const request  = require('supertest-as-promised');
+const should = require('chai').should();
+const sinon = require('sinon');
+const request = require('supertest');
 
-const app     = require('../../app');
-const config  = require('konfig')({ path: 'config' });
+const app = require('../../app');
+const config = require('konfig')({path: 'config'});
 const service = require('../../lib/services/example-service');
 
-describe('controllers/example', function() {
+const sandbox = sinon.createSandbox();
 
-  let sandbox;
+describe('controllers/example', () => {
   let mock;
 
-  let basePath = config.app.microservice.server.name;
+let basePath = config.app.microservice.server.name;
 
-  let examples = [{
-    "id": 1,
-    "name": "movies"
-  }, {
-    "id": 2,
-    "name": "shows"
-  }];
+let examples = [{
+  'id': 1,
+  'name': 'movies'
+}, {
+  'id': 2,
+  'name': 'shows'
+}];
 
-  beforeEach(function(done) {
-    sandbox = sinon.sandbox.create();
-    mock    = sandbox.mock(service);
-    done();
-  });
+beforeEach(() => {
+  mock = sandbox.mock(service);
+});
 
-  afterEach(function(done) {
-    sandbox.restore();
-    done();
-  });
+afterEach(() => {
+  sandbox.restore();
+});
 
-  it('should return a list of Examples when GET /example is requested', function(done) {
+it('should return a list of Examples when GET /example is requested', () => {
+  mock.expects('getExamples').resolves(examples);
 
-    mock.expects('getExamples').resolves(examples);
+return request(app)
+    .get('/' + basePath + '/example')
+    .set('Accept', 'application/json')
+    .expect(200)
+    .then((response) => {
+  response.body[0].id.should.equal(1);
+response.body[0].name.should.equal('movies');
+response.body[1].id.should.equal(2);
+response.body[1].name.should.equal('shows');
+})
+.catch((err) => {
+  console.log(err);
+});
+});
 
-    request(app)
-        .get('/' + basePath + '/example')
-        .set('Accept', 'application/json')
-        .expect(function(res) {
-          res.body[0].id.should.equal(1);
-          res.body[0].name.should.equal('movies');
-          res.body[1].id.should.equal(2);
-          res.body[1].name.should.equal('shows');
-        })
-        .expect(200, done);
-  });
-
-  it('should return a Error when GET /example/error is requested', function(done) {
-
-    mock.expects('throwError').rejects('bad');
-
-    request(app)
+it('should return a Error when GET /example/error is requested', () => {
+  return request(app)
       .get('/' + basePath + '/example/error')
       .set('Accept', 'application/json')
-      .expect(500, done);
-  });
+      .expect(404);
+});
 });
