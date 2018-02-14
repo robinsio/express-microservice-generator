7fb4974cd502a8c76f4b38e308c1b86891e52488
Simon Lamb <simonwlamb@gmail.com>
2018-02-14 17:21:40 +0000

    Improve example coverage in the example controller test

--- generators/app/templates/test/controllers/example.test.js
+++ generators/app/templates/test/controllers/example.test.js
@@ -1,6 +1,5 @@
-'use strict';
+require('chai').should();
 
-const should = require('chai').should();
 const sinon = require('sinon');
 const request = require('supertest');
 
@@ -13,46 +12,81 @@ const sandbox = sinon.createSandbox();
 describe('controllers/example', () => {
   let mock;
 
-let basePath = config.app.microservice.server.name;
+  let basePath = config.app.microservice.server.name;
 
-let examples = [{
-  'id': 1,
-  'name': 'movies'
-}, {
-  'id': 2,
-  'name': 'shows'
-}];
+  let examples = [{
+    'id': 1,
+    'name': 'bands'
+  }, {
+    'id': 2,
+    'name': 'concerts'
+  }];
 
-beforeEach(() => {
-  mock = sandbox.mock(service);
-});
+  beforeEach(() => {
+    mock = sandbox.mock(service);
+  });
 
-afterEach(() => {
-  sandbox.restore();
-});
+  afterEach(() => {
+    sandbox.restore();
+  });
 
-it('should return a list of Examples when GET /example is requested', () => {
-  mock.expects('getExamples').resolves(examples);
+  it('should return a list of Examples when GET /example is requested', () => {
+    return request(app)
+      .get('/' + basePath + '/example')
+      .set('Accept', 'application/json')
+      .expect(200)
+      .then((response) => {
+        response.body[0].id.should.equal(1);
+        response.body[0].name.should.equal('movies');
+        response.body[1].id.should.equal(2);
+        response.body[1].name.should.equal('shows');
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  });
 
-return request(app)
-    .get('/' + basePath + '/example')
-    .set('Accept', 'application/json')
-    .expect(200)
-    .then((response) => {
-  response.body[0].id.should.equal(1);
-response.body[0].name.should.equal('movies');
-response.body[1].id.should.equal(2);
-response.body[1].name.should.equal('shows');
-})
-.catch((err) => {
-  console.log(err);
-});
-});
+  it('should return a list of mocked Examples when GET /example is requested', () => {
+    mock.expects('getExamples').resolves(examples);
 
-it('should return a Error when GET /example/error is requested', () => {
-  return request(app)
+    return request(app)
+      .get('/' + basePath + '/example')
+      .set('Accept', 'application/json')
+      .expect(200)
+      .then((response) => {
+        response.body[0].id.should.equal(1);
+        response.body[0].name.should.equal('bands');
+        response.body[1].id.should.equal(2);
+        response.body[1].name.should.equal('concerts');
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  });
+
+  it('should return a Error when GET /example/error is requested', () => {
+    return request(app)
       .get('/' + basePath + '/example/error')
       .set('Accept', 'application/json')
       .expect(404);
-});
+  });
+
+  it('should return a ResourceNotFoundError when GET /example with a bad sub-uri', () => {
+    mock.expects('getExamples').resolves();
+
+    return request(app)
+      .get('/' + basePath + '/example/bad')
+      .set('Accept', 'application/json')
+      .expect(404);
+  });
+
+  it('should return a Bad Request response when GET /example results in underlying service exception', () => {
+    mock.expects('getExamples').rejects('CustomError');
+
+    return request(app)
+      .get('/' + basePath + '/example')
+      .set('Accept', 'application/json')
+      .expect(500);
+  });
+
 });
