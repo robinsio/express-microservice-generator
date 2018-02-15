e6e1c1efbe60701f69ab8c3bf34c51e0e662c8fb
Simon Lamb <simonwlamb@gmail.com>
2018-02-15 08:48:21 +0000

    Add some test coverage

--- test/test-app.js
+++ test/test-app.js
@@ -1,22 +1,25 @@
 'use strict';
 
 var path = require('path');
-var assert = require('yeoman-generator').assert;
-var helpers = require('yeoman-generator').test;
+var assert = require('yeoman-assert');
+var helpers = require('yeoman-test');
+
 var os = require('os');
 
 describe('microservice:app', function () {
 
-  before(function (done) {
-    helpers.run(path.join(__dirname, '../generators/app'))
-      .withOptions({ skipInstall: true })
-      .withPrompts({ someOption: true })
-      .on('end', done);
-  });
-
+  // TODO - Add more test coverage
   it('creates files', function () {
-    assert.file([
-      'package.json'
-    ]);
+    return helpers.run(path.join(__dirname, '../generators/app'))
+        .withOptions({ skipInstall: true })
+        .withPrompts({ name: 'test-v1', microservice: 'test/v1', entitlements: true, nvmrc: true })
+        .then(function (dir) {
+          assert.file('test-v1/app.js');
+          assert.file('test-v1/package.json');
+          assert.file('test-v1/README.md');
+          assert.file('test-v1/.nvmrc');
+          assert.file('test-v1/.gitignore');
+          assert.file('test-v1/.eslintrc');
+        });
   });
 });
